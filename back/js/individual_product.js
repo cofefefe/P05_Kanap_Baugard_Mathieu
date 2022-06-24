818777484236f86f8e9b49b75c3c1266aaa5aa2f
Mathieu Baugard <mathieu.baugard@g-mail.fr>
2022-06-24 12:55:42 +0200

    individual product folio kanap

--- back/js/individual_product.js
+++ back/js/individual_product.js
@@ -1,14 +1,30 @@
 let site = document.location;
-let url = new URL(site);
-let productId = url.searchParams.get("id");
+let link = new URL (site)
+let productId = link.searchParams.get("id");
 
-function displayProductImage(product) {
-    // Display Product image
-    let img = document.createElement('img');
-    img.src = product.imageUrl;
-    img.alt = product.altTxt;
+fetch('http://localhost:3000/api/products/' + productId)
+    .then(res => res.json())
+    .then((data) => {
+
+    let img = document.createElement('img')
+    img.src = data.imageUrl
+    img.alt = data.altTxt;
+
     let itemImg = document.querySelector('.item__img');
     itemImg.appendChild(img);
-}
 
-displayProductImage()
+    let description = document.getElementById('description');
+    description.textContent = data.description;
+
+    let price = document.getElementById('price')
+    price.textContent = data.price
+
+    let colors = document.getElementById('colors');
+        for (let color of data.colors) {
+            let option = document.createElement('option');
+            option.value = color;
+            option.textContent = color;
+            colors.appendChild(option);
+        }
+})
+
