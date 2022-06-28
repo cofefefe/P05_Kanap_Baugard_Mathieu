080730c434936229b3bb3fcc766d840e6fa45acf
Mathieu Baugard <mathieu.baugard@g-mail.fr>
2022-06-28 14:30:34 +0200

    Initialization local storage

--- back/js/cart.js
+++ back/js/cart.js
@@ -1,39 +1,39 @@
-const addKanap = document.getElementById("addToCart")
+const addProduct = document.getElementById('addToCart')
 
-fetch('http://localhost:3000/api/products')
-    .then(res => res.json())
-    .then((data) => {
 
-    // Basket save //
-    function saveBasket () {
-        localStorage.setItem("basket", JSON.stringify(basket))
-    }
-
-    /// Récupération des données sauvegardées par local storage ///
-    function getBasket() {
-    let basket = localStorage.getItem("product.html?id=" + data._id)
+/// Sauvegarde du panier selectionné ///
+function saveBasket (basket){
+    localStorage.setItem("basket", JSON.stringify(basket))
+}
+/// Récupération des données sauvegardées par local storage ///
+function getBasket() {
+    let basket = localStorage.getItem("product.html?id=" + productId)
     if(basket == null){
         return [];
     }else{
         return JSON.parse(basket);
     }}
+/// Ajout de données dans le localStorage par le client ///
 
     function addBasket(product) {
         fetch('http://localhost:3000/api/products')
         .then(res => res.json())
-        .then((data) => {
+        .then((product) => {
  
         let basket = getBasket()
-        let findProduct = basket.find(p => p.id == "product.html?id=");
-        value = product._id
+        let findProduct = basket.find(p => p.id == "product.html?id=" + productId);
+        value = productId
         if(findProduct != undefined){
             findProduct.quantity++
         }else{
             product.quantity = 1
             basket.push(product)
         }
-    })}
-    /// suppression de donnée dans le localStorage par le client ///
+        saveBasket(basket)
+})}
+
+
+/// suppression de donnée dans le localStorage par le client ///
 function removeBasket(product){
     let basket = getBasket()
     basket = basket.filter(p => p.id != product.id)
@@ -42,7 +42,7 @@ function removeBasket(product){
 /// controle de la quantité pour avoir des sommes pertinentes ( pas de -1 ) ///
 function changeQuantity(product, quantity){
     let basket = getBasket();
-    let findProduct = basket.find(p => p.id == "product.html?id=" + data._id)
+    let findProduct = basket.find(p => p.id == "product.html?id=" + productId)
     if (findProduct != undefined) {
         findProduct.quantity += quantity
         if(findProduct.quantity <= 0){
@@ -61,23 +61,20 @@ function numberProduct() {
     }
     return number
 }
-/// Multiplicateur pour afficher prix correct ///
+/// Multiplicateur pour afficher prix correct au niveau du panier ///
 function totalPrice() {
     let basket = getBasket()
     let number = 0
     for(let product of basket){
         number += product.quantity * product.price
     }
+    document.getElementById('cart__price')
     return number
 }
 
-
-})
-addKanap.addEventListener('click', () => {
+addProduct.addEventListener('click', () => {
     saveBasket()
     addBasket()
-    changeQuantity()
     numberProduct()
     totalPrice()
 })
-
