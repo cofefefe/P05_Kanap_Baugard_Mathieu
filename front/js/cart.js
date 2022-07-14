7fd570d3ef2768173dc04831f6506679213d2209
Mathieu Baugard <mathieu.baugard@g-mail.fr>
2022-07-14 08:43:38 +0200

    display product, forms regex

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,12 +3,9 @@ let productsFromLocalStorage = localStorage.getItem('products');
 // On converti les produits qui sont de type "texte" en tableau pour pouvoir utiliser la fonction forEach ensuite par exemple
 let productsFromLocalStorageArray = JSON.parse(productsFromLocalStorage);
 // on déclare une variable qui va gérer le prix total de notre commande
-let totalPrice = 0
 let sectionCartItemsEl = document.getElementById('cart__items');
 // on récupère le modèle de l'article
-let cartItemPrototypeEl = document.querySelector('.cart__item');
 // on récupère le positionnement de l'image sur la page html
-let displayImgOnBasketPage = document.getElementsByClassName("cart__item__img")
 
 
 // On fait un appel a l'API pour récupérer les données manquantes des options produits (imageUrl, prix)
@@ -16,23 +13,44 @@ fetch('http://localhost:3000/api/products/')
     .then(res => res.json())
     .then((products) => {
 
-        // boucle d'affichage des produits. On fait une première boucle dans l'api puis dans le local storage
-        // Pour chaque produit, on créé un article avec structure html définie, et on lui donne ses données à y afficher
-        products.forEach(product => {
-            productsFromLocalStorageArray.forEach(localProduct => {
-                if(localProduct.id === product._id){
-                    // création d'article dans la section voulue, pour chaque produit dans le local storage
-                    sectionCartItemsEl.appendChild(createArticle(product, localProduct.quantity, localProduct.color))
-                    totalPrice += product.price
-                }
-            });
-        });
-    displayTotalPrice(totalPrice)
+        let articles = displayArticles(products);
+        displayTotalPrice(articles);
+
+        // Si l'une de ces vérifications est false, alors on annule l'actualisation de la page
+        document.getElementById('order').addEventListener('click', function verifyInfoClientOrder(e){
+            e.preventDefault()
+            if (!formIsValid()) {
+                return;
+            }
+            document.location = "confirmation.html";
+        })
+
+
 })
+
+function displayArticles(products) {
+    let articles = [];
+    // boucle d'affichage des produits. On fait une première boucle dans l'api puis dans le local storage
+    // Pour chaque produit, on créé un article avec structure html définie, et on lui donne ses données à y afficher
+    products.forEach(product => {
+        productsFromLocalStorageArray.forEach(localProduct => {
+            if (localProduct.id === product._id){
+                // création d'article dans la section voulue, pour chaque produit dans le local storage
+                sectionCartItemsEl.appendChild(createArticle(product, localProduct.quantity, localProduct.color))
+                articles.push(product);
+            }
+        });
+    });
+    return articles;
+}
+
 // Affichage du prix total de la commande
-function displayTotalPrice(totalPrice){
-    const displayPrice = document.getElementById("totalPrice")
-    displayPrice.textContent = totalPrice
+function displayTotalPrice(articles){
+    let totalPrice = 0;
+    articles.forEach(function (article) {
+        totalPrice += article.price;
+    });
+    document.getElementById("totalPrice").textContent = totalPrice
 }
 // On génère une aboresence html qui se reproduira pour chaque produit ainsi que les classes pour adapter le style
 function createArticle (product, quantity, color) {
@@ -40,22 +58,24 @@ function createArticle (product, quantity, color) {
     newArticle.classList.add("cart__item")
     newArticle.dataset.id = product._id
     newArticle.dataset.color = color
-    
+
+    // Image
     let imageContainer = newArticle.appendChild(document.createElement("div"))
     imageContainer.classList.add("cart__item__img") 
     let image = imageContainer.appendChild(document.createElement("img"))
     image.setAttribute('alt', product.altTxt)
     image.setAttribute('src', product.imageUrl)
 
+    // Description
     let cartItemContainer = newArticle.appendChild(document.createElement("div"))
     cartItemContainer.classList.add("cart__item__content")
     let cartItemDescription = cartItemContainer.appendChild(document.createElement("div"))
     cartItemDescription.classList.add("cart__item__content__description")
     cartItemDescription.textContent = product.description
 
+    // Settings
     let cartItemContentSetting = cartItemContainer.appendChild(document.createElement("div"))
     cartItemContentSetting.classList.add("cart__item__content__settings")
-
 
     let cartItemQuantity = cartItemContentSetting.appendChild(document.createElement("div"))
     cartItemQuantity.classList.add("cart__item__content__settings__quantity")
@@ -77,39 +97,15 @@ function createArticle (product, quantity, color) {
     return newArticle
 }
 
-//**** Gestion de la supression d'article du Local Storage *****//
-
-//**** Gestion de donnée du formulaire client *****//
-// email et erreur msg
-let emailInput = document.getElementById("email")
-let emailErrorMsg = document.getElementById("emailErrorMsg")
-// firstname et erreur msg
-let firstNameClient = document.getElementById("firstName")
-let firstNameErrorMsg = document.getElementById("firstNameErrorMsg")
-// lastname et erreur msg
-let lastNameClient = document.getElementById("lastName")
-let lastNameErrorMsg = document.getElementById("lastNameErrorMsg")
-// adresse et erreur msg
-let addressErrorMsg = document.getElementById("adressErrorMsg")
-let adressClient = document.getElementById("adress")
-
-
 // Paramètrage de différents regex : email, ville/nom/prenom, adresse
 let regexEmail = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9-_]+[.]{1}[a-z]{2,10}$')
 let regexName = new RegExp('^[a-z,.-]{2,20}$')
 let regexAdress = new RegExp("^[a-zA-Z0-9\s,-]*$")
 
-// La variable contact sera la fiche contact du client, représentant toutes les clés necessaire et données saisies
-let contact = {
-    firstName: document.getElementById("firstName").value,
-    lastName: document.getElementById("lastName").value,
-    address: document.getElementById("address").value,
-    city: document.getElementById("city").value,
-    email: document.getElementById("email").value,
-};
 
 // Validation d'adresse email //
-function clientEmailVerification () {
+function clientEmailVerification (contact) {
+    let emailErrorMsg = document.getElementById("emailErrorMsg")
 if (regexEmail.test(contact.email) === false) {
     emailErrorMsg.textContent = 'Veuillez saisir une adresse Email valide';
     return false;
@@ -120,7 +116,8 @@ return true;
 }
 
 // Validation de nom/prénom
-function clientFirstNameVerification(){
+function clientFirstNameVerification(contact){
+    let firstNameErrorMsg = document.getElementById("firstNameErrorMsg")
 if (regexName.test(contact.firstName) === false){
     firstNameErrorMsg.textContent = 'Veuillez saisir un prénom valide'
     return false
@@ -129,7 +126,8 @@ if (regexName.test(contact.firstName) === false){
 } 
 return true
 }
-function clientLastNameVerification(){
+function clientLastNameVerification(contact){
+    let lastNameErrorMsg = document.getElementById("lastNameErrorMsg")
     if (regexName.test(contact.lastName) === false){
         lastNameErrorMsg.textContent = 'Veuillez saisir un nom valide'
         return false
@@ -140,7 +138,8 @@ function clientLastNameVerification(){
 }
 
 // Validation d'adresse
-function clientAdressVerification(event){
+function clientAdressVerification(contact){
+    let addressErrorMsg = document.getElementById("addressErrorMsg")
     if (regexAdress.test(contact.address) === false){
         addressErrorMsg.textContent = 'Veuillez saisir une adresse valide'
         return false
@@ -151,11 +150,10 @@ function clientAdressVerification(event){
 }
 
 // Validation de ville, comme les prénoms pas de chiffres ni de symbole, on utilise le regex name
-function clientcityVerification(event){
+function validateClientCity(contact){
+    let cityErrorMsg = document.getElementById("cityErrorMsg");
     if (regexName.test(contact.city) === false){
         cityErrorMsg.textContent = 'Veuillez saisir une ville valide'
-        event.preventDefault()
-
         return false
     } else {
         cityErrorMsg.textContent = ''
@@ -163,16 +161,33 @@ function clientcityVerification(event){
     return true
 }
 
-// Si l'une de ces vérifications est false, alors on annule l'actualisation de la page
-document.getElementById('order').addEventListener('click', function verifyInfoClientOrder(e){
 
-    if(
-          clientcityVerification() == false 
-       || clientAdressVerification() == false 
-       || clientFirstNameVerification() == false 
-       || clientLastNameVerification() == false 
-       || clientEmailVerification() == false
-       ){
-            e.preventDefault()
-        }
-})
+function formIsValid() {
+    let formIsValid = true;
+
+    // La variable contact sera la fiche contact du client, représentant toutes les clés necessaire et données saisies
+    let contact = {
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        address: document.getElementById("address").value,
+        city: document.getElementById("city").value,
+        email: document.getElementById("email").value,
+    };
+
+    if (!validateClientCity(contact)) {
+        formIsValid = false;
+    }
+    if (!clientAdressVerification(contact)) {
+        formIsValid = false;
+    }
+    if (!clientFirstNameVerification(contact)) {
+        formIsValid = false;
+    }
+    if (!clientLastNameVerification(contact)) {
+        formIsValid = false;
+    }
+    if (!clientEmailVerification(contact)) {
+        formIsValid = false;
+    }
+    return formIsValid;
+}
