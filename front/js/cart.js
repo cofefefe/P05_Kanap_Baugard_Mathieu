e99f08ffc5b1cf9d63bee39437db3a2083dcdf62
Mathieu Baugard <mathieu.baugard@g-mail.fr>
2022-07-28 10:19:23 +0200

    managing deletion by client

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,27 +1,16 @@
-// On récupère les produits enregistrés dans le local storage
-let productsFromLocalStorage = localStorage.getItem('products');
-// On converti les produits qui sont de type "texte" en tableau pour pouvoir utiliser la fonction forEach ensuite par exemple
-let productsFromLocalStorageArray = JSON.parse(productsFromLocalStorage);
 // on déclare une variable qui va gérer le prix total de notre commande
 let sectionCartItemsEl = document.getElementById('cart__items');
+let productCollection = [];
 
-let getProductFromLocalStorage = JSON.parse(localStorage.getItem("products"))
-
-function getProduct(){
-    if(productsFromLocalStorageArray){
-        JSON.parse(localStorage.getItem("products"))
-        return true
-    }else{
-        return false
-    }
-}
 // On fait un appel a l'API pour récupérer les données manquantes des options produits (imageUrl, prix)
 fetch('http://localhost:3000/api/products/')
     .then(res => res.json())
     .then((products) => {
-
-        let articles = displayArticles(products);
-        displayTotalPrice(articles);
+        productCollection = products;
+        let productsFromLocalStorageArray = getProductsFromLocalStorage();
+        displayArticles(products, productsFromLocalStorageArray);
+        displayEmptyCollection(productsFromLocalStorageArray);
+        displayTotalPrice(productsFromLocalStorageArray);
 
         // Si l'une de ces vérifications est false, alors on annule l'actualisation de la page
         document.getElementById('order').addEventListener('click', function (e) {
@@ -31,43 +20,64 @@ fetch('http://localhost:3000/api/products/')
             }
             document.location = "confirmation.html";
         })
+    });
 
-})
+function displayEmptyCollection(articles) {
+    if (articles.length === 0) {
+        const elemPanierVide = `<div style="display:flex; margin:auto; justify-content:center"><p style="font-size:40px; padding: 30px">Votre panier est vide</p></div>`;
+        const elem = document.querySelector("#cartAndFormContainer h1")
+        elem.insertAdjacentHTML("afterend", elemPanierVide)
+    }
+}
 
-function displayArticles(products) {
+function getProductsFromLocalStorage() {
+    // On récupère les produits enregistrés dans le local storage
+    let productsFromLocalStorage = localStorage.getItem('products');
+    // On converti les produits qui sont de type "texte" en tableau pour pouvoir utiliser la fonction forEach ensuite par exemple
+    return JSON.parse(productsFromLocalStorage);
+}
+
+function displayArticles(products, productsFromLocalStorageArray) {
     let articles = [];
     // boucle d'affichage des produits. On fait une première boucle dans l'api puis dans le local storage
     // Pour chaque produit, on créé un article avec structure html définie, et on lui donne ses données à y afficher
     products.forEach(product => {
         productsFromLocalStorageArray.forEach(localProduct => {
-            if (localProduct.id === product._id){
+            if (localProduct.id === product._id) {
                 // création d'article dans la section voulue, pour chaque produit dans le local storage
                 sectionCartItemsEl.appendChild(createArticle(product, localProduct.quantity, localProduct.color))
                 articles.push(product);
             }
         });
     });
-    displayLengthArticles()
+    displayLengthArticles(productsFromLocalStorageArray)
     deleteItemFromLocalStorage()
     managingQuantityByClient()
     return articles;
 }
 
 // Affichage du prix total de la commande
-function displayTotalPrice(articles){
+function displayTotalPrice(articles) {
     let totalPrice = 0;
     articles.forEach(function (article) {
-        totalPrice += article.price;
+        const productFound = findProductById(article.id);
+        totalPrice += productFound.price * article.quantity;
     });
     document.getElementById("totalPrice").textContent = totalPrice
 }
+
+function findProductById(productId) {
+    let productFound;
+    productCollection.forEach(function (product) {
+        if (product._id === productId) {
+            productFound = product;
+        }
+    });
+    return productFound;
+}
+
 // On génère une aboresence html qui se reproduira pour chaque produit ainsi que les classes pour adapter le style
-function createArticle (product, quantity, color) {
-    if(getProduct == false){
-        const elemPanierVide = `<div style="display:flex; margin:auto; justify-content:center"><p style="font-size:40px; padding: 30px">Votre panier est vide</p></div>`;
-        const elem = document.querySelector("#cartAndFormContainer h1")
-        elem.insertAdjacentHTML("afterend", elemPanierVide)
-    }else{    
+function createArticle(product, quantity, color) {
     let newArticle = document.createElement("article")
     newArticle.classList.add("cart__item")
     newArticle.dataset.id = product._id
@@ -75,7 +85,7 @@ function createArticle (product, quantity, color) {
 
     // Image
     let imageContainer = newArticle.appendChild(document.createElement("div"))
-    imageContainer.classList.add("cart__item__img") 
+    imageContainer.classList.add("cart__item__img")
     let image = imageContainer.appendChild(document.createElement("img"))
     image.setAttribute('alt', product.altTxt)
     image.setAttribute('src', product.imageUrl)
@@ -109,69 +119,71 @@ function createArticle (product, quantity, color) {
     deleteItem.textContent = "Supprimer"
 
     return newArticle
-    }
 }
-
 
 
 // Paramètrage de différents regex : email, ville/nom/prenom, adresse
 let regexEmail = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9-_]+[.]{1}[a-z]{2,10}$')
-let regexName = new RegExp('^[a-z,.-]{2,20}$')
-let regexAddress = new RegExp("^[a-zA-Z0-9\s,-]*$")
+let regexName = new RegExp('^[a-zA-Z,.-]{2,20}$')
+let regexAddress = new RegExp("[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+")
 
 
 // Validation d'adresse email //
-function clientEmailVerification (contact) {
+function clientEmailVerification(contact) {
     let emailErrorMsg = document.getElementById("emailErrorMsg")
-if (regexEmail.test(contact.email) === false) {
-    emailErrorMsg.textContent = 'Veuillez saisir une adresse Email valide';
-    return false;
-} else {
-    emailErrorMsg.textContent = '';
+    if (regexEmail.test(contact.email) === false) {
+        emailErrorMsg.textContent = 'Veuillez saisir une adresse Email valide';
+        return false;
+    } else {
+        emailErrorMsg.textContent = '';
+    }
+    return true;
 }
-return true;
-}
+
 // Validation de nom/prénom
-function clientFirstNameVerification(contact){
+function clientFirstNameVerification(contact) {
     let firstNameErrorMsg = document.getElementById("firstNameErrorMsg")
-if (regexName.test(contact.firstName) === false){
-    firstNameErrorMsg.textContent = 'Veuillez saisir un prénom valide'
-    return false
-} else {
-    firstNameErrorMsg.textContent = ''
-} 
-return true
+    if (regexName.test(contact.firstName) === false) {
+        firstNameErrorMsg.textContent = 'Veuillez saisir un prénom valide'
+        return false
+    } else {
+        firstNameErrorMsg.textContent = ''
+    }
+    return true
 }
-function clientLastNameVerification(contact){
+
+function clientLastNameVerification(contact) {
     let lastNameErrorMsg = document.getElementById("lastNameErrorMsg")
-    if (regexName.test(contact.lastName) === false){
+    if (regexName.test(contact.lastName) === false) {
         lastNameErrorMsg.textContent = 'Veuillez saisir un nom valide'
         return false
     } else {
         lastNameErrorMsg.textContent = ''
-    } 
+    }
     return true
 }
+
 // Validation d'adresse
-function clientAddressVerification(contact){
+function clientAddressVerification(contact) {
     let addressErrorMsg = document.getElementById("addressErrorMsg")
-    if (regexAddress.test(contact.address) === false){
+    if (regexAddress.test(contact.address) === false) {
         addressErrorMsg.textContent = 'Veuillez saisir une adresse valide'
         return false
     } else {
         addressErrorMsg.textContent = ''
-    } 
+    }
     return true
 }
+
 // Validation de ville, comme les prénoms pas de chiffres ni de symbole, on utilise le regex name
-function validateClientCity(contact){
+function validateClientCity(contact) {
     let cityErrorMsg = document.getElementById("cityErrorMsg");
-    if (regexName.test(contact.city) === false){
+    if (regexName.test(contact.city) === false) {
         cityErrorMsg.textContent = 'Veuillez saisir une ville valide'
         return false
     } else {
         cityErrorMsg.textContent = ''
-    } 
+    }
     return true
 }
 
@@ -205,19 +217,21 @@ function formIsValid() {
     }
     return formIsValid;
 }
+
 // Montrer le nombre d'article différents présents dans la commande client
-function displayLengthArticles(){
+function displayLengthArticles(productsFromLocalStorageArray) {
     let LengthArticles = document.getElementById('totalQuantity')
     LengthArticles.textContent = productsFromLocalStorageArray.length
 }
+
 ///******* Gestion de quantité, client peut changer quantité de produit sur la page de commande ********///
 function managingQuantityByClient() {
     let btnClientManageQuantity = document.querySelectorAll(".itemQuantity");
 
-    for (let k= 0; k < btnClientManageQuantity.length; k++){
+    for (let k = 0; k < btnClientManageQuantity.length; k++) {
         let btnClientManageQuantity = document.querySelectorAll(".itemQuantity");
 
-        btnClientManageQuantity[k].addEventListener("change" , (event) => {
+        btnClientManageQuantity[k].addEventListener("change", (event) => {
             event.preventDefault();
             let quantityEl = event.target;
             let productEl = quantityEl.closest('article');
@@ -227,7 +241,7 @@ function managingQuantityByClient() {
             let quantity = parseInt(quantityEl.value);
 
             // On modifie seulement la quantité de l'élement selectionné
-
+            let productsFromLocalStorageArray = getProductsFromLocalStorage();
             // On repère quel est l'élement selectionné
             const productFromLocalStorage = productsFromLocalStorageArray.find((product) => {
                 return product.id === productId && product.color === productColor;
@@ -236,6 +250,8 @@ function managingQuantityByClient() {
             // On en modifie la quantité
             productFromLocalStorage.quantity = quantity;
             localStorage.setItem("products", JSON.stringify(productsFromLocalStorageArray));
+
+            displayTotalPrice(productsFromLocalStorageArray);
         })
     }
 }
@@ -243,25 +259,38 @@ function managingQuantityByClient() {
 ///****** Supression de produit, client peut supprimer produit du ls de la page commande ******///
 function deleteItemFromLocalStorage() {
     // on se saisit des boutons sur la page
-        let btnDeleteItemFromLocalStorage = document.querySelectorAll('.deleteItem')
-     
-   // On test si on repère leur présence, c'est le cas
-        console.log(btnDeleteItemFromLocalStorage)
-     
+    let btnDeleteItemFromLocalStorage = document.querySelectorAll('.deleteItem')
+
+    // On test si on repère leur présence, c'est le cas
+
     // maintenant on créé une fonction pour chaque bouton
     btnDeleteItemFromLocalStorage.forEach(btn => {
         // Au clic
-        btn.addEventListener("click", ()=>{
-        // On utilise la méthode filter pour selectionner des produits
+        btn.addEventListener("click", (e) => {
+            let articleEl = e.target.closest('article');
+            const productId = articleEl.dataset.id;
+            const color = articleEl.dataset.color;
 
-            let selectedProduct = 
-            productsFromLocalStorageArray.filter(product => product.id == productsFromLocalStorageArray.id && product.color == productsFromLocalStorageArray.color)
-            console.log(productsFromLocalStorageArray[l].id)
+            // On utilise la méthode filter pour selectionner des produits
+            // 1. On supprime l'élément dans le local storage
+            let productsFromLocalStorageArray = getProductsFromLocalStorage();
+            let productsFromLocalStorageArrayUpdated = productsFromLocalStorageArray.filter(product => {
+                if (productId === product.id && color === product.color) {
+                    return false
+                } else {
+                    return true;
+                }
+            });
+            localStorage.setItem("products", JSON.stringify(productsFromLocalStorageArrayUpdated));
+
+            // 2. On supprime l'élément du DOM
+            articleEl.remove();
+
+            // 3. Display empty message if the collection is empty
+            displayEmptyCollection(productsFromLocalStorageArrayUpdated);
+
+            // 4. Display total price
+            displayTotalPrice(productsFromLocalStorageArrayUpdated);
         })
-    })    
-    
-}
-
-for(let l = 0 ; l < btnDeleteItemFromLocalStorage.length ; l++){
-
+    })
 }
